0d360b2caf2025bb791c7bb73e35cb90457e23b7
Pratheek <apratheekreddy@gmail.com>
2019-03-04 23:52:58 -0500

    File Manager

--- FileManager.py
+++ FileManager.py
@@ -1,15 +1,15 @@
 import os
 import shutil
 
-print("Please enter you command:")
-print("HINT:\nCreate file - fcreate <FNAME>")
-print("Delete file - fdelete <FNAME>")
-print("Copy file - fcopy <FNAME> <FNAME>")
-print("Move file - fmove <FNAME> <DESTINATION>")
-print("Create directory - newdir <FNAME>")
-print("Delete directory - deldir <FNAME>")
-print("Copy directory - copydir <FNAME> <FNAME>")
-print("Move directory - movedir <FNAME> <DESTINATION>")
+print("Please enter your command: (See Below for the commands)")
+print("HINT:\nCreate file - file_create <FNAME>")
+print("Delete file - file_delete <FNAME>")
+print("Copy file - file_copy <FNAME> <FNAME>")
+print("Move file - file_move <FNAME> <DESTINATION>")
+print("Create directory - create_dir <FNAME>")
+print("Delete directory - delete_dir <FNAME>")
+print("Copy directory - copy_dir <FNAME> <FNAME>")
+print("Move directory - move_dir <FNAME> <DESTINATION>")
 
 str = input().split()
 if str == []:
@@ -19,46 +19,46 @@ try:
     str[1]
 except:
     print("Something wrong with command and arguments")
-if str[0] == "fcreate":
+if str[0] == "file_create":
     fname = str[1]
     open(fname, 'a')
-elif str[0] == "fdelete":
+elif str[0] == "file_delete":
     A = os.listdir()
     if str[1] in A:
         os.remove(str[1])
         print(str[1])
     else:
         print("No such file")
-elif str[0] == "newdir":
+elif str[0] == "create_dir":
     A = os.listdir()
     try:
         os.mkdir(str[1])
     except:
         print("There is a directory with such name")
-elif str[0] == "deldir":
+elif str[0] == "delete_dir":
     try:
         shutil.rmtree(str[1])
     except:
         print("There is no such directory")
-elif str[0] == "fcopy":
+elif str[0] == "file_copy":
     try:
         shutil.copy(str[1], str[2])
     except:
         print("Something went wrong")
-elif str[0] == "fmove":
+elif str[0] == "file_move":
     try:
         shutil.move(str[1], str[2])
     except:
         print("Something went wrong")
-elif str[0] == "copydir":
+elif str[0] == "copy_dir":
     try:
         shutil.copytree(str[1], str[2])
     except:
         print("Something went wrong")
-elif str[0] == "movedir":
+elif str[0] == "move_dir":
     try:
         if str[1] == str[2]:
-            print("There is no need in moving")
+            print("No need for moving the directory")
         else:
             shutil.copytree(str[1], str[2])
             shutil.rmtree(str[1])
